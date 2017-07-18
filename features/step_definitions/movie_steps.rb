5dd8ebdf6403b42703f8d59045e7ee2210b5a5fd
Ihor Trypolskyi <ihor.trypolskyi@gmail.com>
2017-07-18 21:09:43 +0000

    fully covered with cucumber

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -8,3 +8,36 @@ Then(/^the director of "([^"]*)" should be "([^"]*)"$/) do |movie_title, directo
     movie = Movie.where(title: movie_title).first
     expect(movie.director).to eql(director)
 end
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  expect(page.body).to have_content /(#{e1})(.+)(#{e2})/m
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/[\s,]/).each do |rating|
+    if uncheck
+      step "I uncheck \"ratings_#{rating}\""
+      # step "I #{uncheck}check \"ratings_#{rating}\""
+    else
+      step "I check \"ratings_#{rating}\""
+    end
+  end
+end
+
+Then /I should see all the movies/ do
+  # Make sure that all the movies in the app are visible in the table
+
+    expect(page.all('table#movies tbody tr').count).to eq Movie.all.count
+    # page.should have_selector('#movies tr', count: 11)
+    # expect(page).to have_selector('#movies tr', count: 11)
+end
+
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -34,18 +34,15 @@ RSpec.describe MoviesController, type: :controller do
             end
             
             it 'does not search for director' do
-                # get :similar, id: 198
                 expect(Movie).to_not receive(:where)
               
             end
             
             it 'redirects to home page' do
-                # get :similar, id: 198
                 expect(response).to redirect_to(movies_path)
             end
             
             it "rises warning" do
-                # get :similar, id: 198
                 expect(flash[:warning]).to match (/has no director info/)
             end
         end
